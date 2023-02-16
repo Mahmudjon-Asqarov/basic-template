4ac7943bd69927e75757a9da3d2f9c82a5709c49
Mahmudjon Asqarov <mahmidjonasqarov101@gmail.com>
2023-02-16 21:41:11 +0500

    fixed

--- src/index.js
+++ src/index.js
@@ -5,12 +5,11 @@ import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
   </Router>
 );
